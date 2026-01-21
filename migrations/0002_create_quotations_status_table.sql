c9d26fef773d7c469f3a1bf8024854c285c05586
Keiner Mendoza <keinermendoza@gmail.com>
2026-01-21 16:37:28 +0000

    feat: create sql migration file for add support to quotation status property

---
+++ migrations/0002_create_quotations_status_table.sql
@@ -0,0 +1,79 @@
+-- Create lokup table for store the available quation status
+CREATE TABLE IF NOT EXISTS "quotation_status" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+-- Alter name table for make changes in foreing key
+ALTER TABLE quotations RENAME TO quotations_temp;
+
+-- Recreate the quotations table with new foreing key
+CREATE TABLE IF NOT EXISTS "quotations" (
+    id INTEGER NOT NULL, 
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    company_name VARCHAR(200) NOT NULL CHECK (company_name != ''), 
+    company_url VARCHAR(300) NOT NULL CHECK (company_url != ''), 
+    product_name VARCHAR(200) NOT NULL CHECK (product_name != ''), 
+    product_url VARCHAR(300) NOT NULL CHECK (product_url != ''), 
+    public_minimum_price VARCHAR(20), 
+    public_minimum_quantity VARCHAR(300), 
+    request_for_quotation_id INTEGER NOT NULL,
+    seller_name VARCHAR(50) NULL,
+    cheapest_shipping_company VARCHAR(8) NULL,
+    cheapest_shipping_cost NUMERIC NULL,
+    unit_product_price_offered NUMERIC NULL,
+    "status" INTEGER,
+    PRIMARY KEY (id), 
+    CONSTRAINT uix_cotation_company_and_product_names UNIQUE (request_for_quotation_id, product_name, company_name), 
+    FOREIGN KEY(request_for_quotation_id) REFERENCES "quotation_status" (id) ON DELETE RESTRICT,
+    FOREIGN KEY(status) REFERENCES "request_for_quotations" (id) ON DELETE CASCADE
+);
+
+-- Populates the quotation status table
+INSERT INTO "quotation_status" ("name")
+VALUES
+    ('just quoted'),
+    ('wating for me'),
+    ('need shipment quotation'),
+    ('complete'),
+    ('discarted'),
+    ('selected');
+
+-- fill the new quotations table with old data and default value
+INSERT INTO "quotations" (
+    id,
+    created,
+    company_name,
+    company_url,
+    product_name,
+    product_url,
+    public_minimum_price,
+    public_minimum_quantity,
+    request_for_quotation_id,
+    seller_name,
+    cheapest_shipping_company,
+    cheapest_shipping_cost,
+    unit_product_price_offered,
+    status
+) SELECT
+    id,
+    created,
+    company_name,
+    company_url,
+    product_name,
+    product_url,
+    public_minimum_price,
+    public_minimum_quantity,
+    request_for_quotation_id,
+    seller_name,
+    cheapest_shipping_company,
+    cheapest_shipping_cost,
+    unit_product_price_offered,
+    (
+        SELECT id FROM quotation_status WHERE name = 'just quoted'
+    ) 
+    FROM quotations_temp;
+
+-- Drop temporary table
+DROP TABLE quotations_temp;
